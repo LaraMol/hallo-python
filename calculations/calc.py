c5f7ea6bb81d08d785913f700e082fd77bba72e1
LaraMol <99068065@mydavinci.nl>
2021-09-06 12:17:10 +0200

    hallo-python

---
+++ calculations/calc.py
@@ -0,0 +1,18 @@
+print ('college geld voor 23 leerlingen:')
+print ('per jaar kost het voor een leerling 1202 euro')
+since = 1202
+up = 23
+result = up * since
+print(str(result)+ ' Voor alle leerlingen in 1 jaar') 
+
+
+print('btw voor appels, druiven en bananen.')
+print ('appels kost 3,40 euro')
+print ('druiven 2,45 euro')
+print ('bananen voor 1.95 euro')
+calculate = 3.40 + 2.45 + 1.95 
+print (str(calculate))
+
+calculate = 3.40 + 2.45 + 1.95 / 100 * 6
+print ('En nu btw in totaal')
+print (str(calculate))
---
+++ calculations/calculations.py
@@ -0,0 +1,4 @@
+
+
+print ('college geld voor 23 leerlingen:')
+print ('per jaar kost het voor een leerling 1202 euro')
